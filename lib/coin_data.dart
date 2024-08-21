0e49d6ae8737d06525741e3c9f06c604cbec7fca
carlos <carlosolivares496@gmail.com>
2024-08-20 22:20:16 -0300

    silver medal

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -32,16 +32,20 @@ const List<String> cryptoList = [
 ];
 
 class CoinData {
-  Future getCoinData(String selectedCurrency) async {
-    String requestURL =
-        'https://rest.coinapi.io/v1/exchangerate/BTC/$selectedCurrency?apikey=C2964A1D-3225-4D9A-B2FB-100C4E6BD86A';
-    http.Response response = await http.get(Uri.parse(requestURL));
-    if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body);
-      double selectedCurrency = jsonResponse['rate'] as double;
-      return selectedCurrency.toStringAsFixed(0);
-    } else {
-      print(response.statusCode);
+  Future<Map<String, String>> getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          'https://rest.coinapi.io/v1/exchangerate/$crypto/$selectedCurrency?apikey=C2964A1D-3225-4D9A-B2FB-100C4E6BD86A';
+      http.Response response = await http.get(Uri.parse(requestURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+      }
     }
+    return cryptoPrices;
   }
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -12,7 +12,6 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
-  late int rate = 0;
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -52,13 +51,16 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  String bitcoinValue = '?';
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
 
   void getData() async {
+    isWaiting = true;
     try {
       var data = await CoinData().getCoinData(selectedCurrency);
+      isWaiting = false;
       setState(() {
-        bitcoinValue = data as String;
+        coinValues = data;
       });
     } catch (e) {
       print(e);
@@ -83,27 +85,34 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              CryptoCard(
+                cryptoCurrency: 'BTC',
+                value: isWaiting
+                    ? '?'
+                    : coinValues['BTC'] ??
+                        '?', // Usa un valor por defecto si es null
+                selectedCurrency: selectedCurrency,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $bitcoinValue $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
+              CryptoCard(
+                cryptoCurrency: 'ETH',
+                value: isWaiting
+                    ? '?'
+                    : coinValues['ETH'] ??
+                        '?', // Usa un valor por defecto si es null
+                selectedCurrency: selectedCurrency,
               ),
-            ),
+              CryptoCard(
+                cryptoCurrency: 'LTC',
+                value: isWaiting
+                    ? '?'
+                    : coinValues['LTC'] ??
+                        '?', // Usa un valor por defecto si es null
+                selectedCurrency: selectedCurrency,
+              ),
+            ],
           ),
           Container(
             height: 150.0,
@@ -113,6 +122,42 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  const CryptoCard(
+      {required this.value,
+      required this.selectedCurrency,
+      required this.cryptoCurrency});
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
